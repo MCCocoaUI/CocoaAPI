cced00fa2f6ddcdee53140d8d18dff0d2a7088a0
hookan <916811837@qq.com>
2018-07-11 19:48:41 +0800

    update

--- src/demo/java/top/hookan/cocoa/demo/core/Hooks.java
+++ src/demo/java/top/hookan/cocoa/demo/core/Hooks.java
@@ -1,6 +1,7 @@
 package top.hookan.cocoa.demo.core;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.TextComponentString;
 import top.hookan.cocoa.core.CocoaTransformer;
 import top.hookan.cocoa.core.annotation.CocoaHook;
 
@@ -12,6 +13,15 @@ public class Hooks extends CocoaTransformer
             type = CocoaHook.HookType.END)
     public static void testFun1(Minecraft mc)
     {
-        System.out.println("玩家攻击！END");
+        if (mc.player != null) mc.player.sendMessage(new TextComponentString("玩家点击鼠标 END"));
+    }
+    
+    @CocoaHook(owner = "net.minecraft.client.Minecraft",
+            mcp = "clickMouse:()V",
+            notch = "aA:()V",
+            type = CocoaHook.HookType.BEGIN)
+    public static void testFun2(Minecraft mc)
+    {
+        if (mc.player != null) mc.player.sendMessage(new TextComponentString("玩家点击鼠标 BEGIN"));
     }
 }
