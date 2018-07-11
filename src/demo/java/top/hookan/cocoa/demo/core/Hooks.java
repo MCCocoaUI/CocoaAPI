90b597fba43d0368751ad5206939b23fa215ef62
kevinwalker <1192085905@qq.com>
2018-07-11 11:43:47 +0800

    测试功能

--- src/demo/java/top/hookan/cocoa/demo/core/Hooks.java
+++ src/demo/java/top/hookan/cocoa/demo/core/Hooks.java
@@ -1,8 +1,16 @@
 package top.hookan.cocoa.demo.core;
 
+import net.minecraft.client.Minecraft;
 import top.hookan.cocoa.core.CocoaTransformer;
+import top.hookan.cocoa.core.annotation.CocoaHook;
 
 public class Hooks extends CocoaTransformer
 {
-
+    @CocoaHook(owner = "net.minecraft.client.Minecraft",
+            mcp = "clickMouse:()V",
+            notch = "clickMouse:()V",
+            type = CocoaHook.HookType.END)
+    public static void testFun1(Minecraft mc){
+        System.out.println("玩家攻击！END");
+    }
 }
